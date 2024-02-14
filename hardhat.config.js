fde58c88dc3a6a61684223cad3f2438111dbb21d
oanskyy <oana.anghelachi@gmail.com>
2024-02-14 20:41:59 +0200

    feat: instal ethers library, update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,25 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.24",
+// };
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("dotenv").config()
+require("@nomiclabs/hardhat-ethers")
+
+const { API_URL, PRIVATE_KEY } = process.env
+
 module.exports = {
-  solidity: "0.8.24",
-};
+	solidity: "0.7.3",
+	defaultNetwork: "sepolia",
+	networks: {
+		hardhat: {},
+		sepolia: {
+			url: API_URL,
+			accounts: [`0x${PRIVATE_KEY}`]
+		}
+	}
+}
