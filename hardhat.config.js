8707cd3699ea8348cf564e20a7e5d038949962fb
oanskyy <oana.anghelachi@gmail.com>
2024-02-15 00:10:04 +0200

    feat: publish your smart contract's ABI and source code to Etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,11 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.24",
-// };
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 
 require("dotenv").config()
 require("@nomiclabs/hardhat-ethers")
+require("@nomiclabs/hardhat-etherscan")
 
 const { API_URL, PRIVATE_KEY } = process.env
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
 	solidity: "0.7.3",
@@ -21,5 +16,12 @@ module.exports = {
 			url: API_URL,
 			accounts: [`0x${PRIVATE_KEY}`]
 		}
+	},
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY
 	}
 }
+
+// module.exports = {
+//   solidity: "0.8.24",
+// };
